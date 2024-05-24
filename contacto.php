e382126b4b3166e8ce95fe5b462946023c4170ea
SebastianDev017 <sruizramirez11@gmail.com>
2024-05-24 17:41:41 +0200

    second commit

--- contacto.php
+++ contacto.php
@@ -1,40 +1,43 @@
 <?php
-// Guardar los datos recibidos en variables:
- if(isset($_POST['nombre'],$_POST['correo'],$_POST['asunto'],$_POST['telefono'],$_POST['mensaje'])){
-  $nombre =$_POST['nombre'];
-  $email = $_POST['correo'];
-  $asunto_msj=$_POST['asunto'];
-  $telefono =$_POST['telefono'];
-  $mensaje = $_POST['mensaje'];
-  
-  // Definir el correo de destino:
-  $dest = "Ocordova@gestionesmartimas.com";  
- 
-  // Estas son cabeceras que se usan para evitar que el correo llegue a SPAM:
-  $headers = "From: $nombre <$email>\r\n";  
-  $headers .= "X-Mailer: PHP5\n";
-  $headers .= 'MIME-Version: 1.0' . "\n";
-  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
- 
-  // Aqui definimos el asunto y armamos el cuerpo del mensaje
-  $asunto = $asunto_msj;
-  $cuerpo = "Nombre: ".$nombre."<br>";
-  $cuerpo .= "Email: ".$email."<br>";
-  $cuerpo .= "Telefono: ".$telefono."<br>";
-  $cuerpo .= "Mensaje: ".$mensaje;
- 
-   // Esta es una pequena validación, que solo envie el correo si todas las variables tiene algo de contenido:
-   if($nombre != '' && $email != '' && $telefono != '' && $mensaje != ''){
-    mail($dest,$asunto,$cuerpo,$headers); //ENVIAR!
-    echo "<center><img src='images/contacto-exito.png' class='animated fadeInRight  img-responsive'></center>";
-    //echo "<h4 class='animated fadeInRight'><center>Su información fue enviada exitosamente</center></h4>";
-    //echo "<h5 class='animated fadeInRight'><center>Grácias por contactarnos <b>$nombre</b>, pronto recibirá noticias nuestras.</center></h5>";
-    echo "<br>";
-    }else{
-	echo "No fue posible enviar al información";
-    }
+if (isset($_POST['nombre'], $_POST['correo'], $_POST['asunto'], $_POST['telefono'], $_POST['mensaje'])) {
+    $nombre = trim($_POST['nombre']);
+    $email = trim($_POST['correo']);
+    $asunto_msj = trim($_POST['asunto']);
+    $telefono = trim($_POST['telefono']);
+    $mensaje = trim($_POST['mensaje']);
 
-    }else{
-	echo "Error al intentar enviar los datos, verificar espacios vacios.";
+    // Validación básica
+    if ($nombre != '' && $email != '' && $asunto_msj != '' && $telefono != '' && $mensaje != '') {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Definir el correo de destino:
+            $dest = "informes@gestionesmaritimas.com";  
+
+            // Estas son cabeceras que se usan para evitar que el correo llegue a SPAM:
+            $headers = "From: " . htmlspecialchars($nombre) . " <" . htmlspecialchars($email) . ">\r\n";  
+            $headers .= "X-Mailer: PHP/" . phpversion() . "\n";
+            $headers .= 'MIME-Version: 1.0' . "\n";
+            $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+            // Aqui definimos el asunto y armamos el cuerpo del mensaje
+            $asunto = htmlspecialchars($asunto_msj);
+            $cuerpo = "Nombre: " . htmlspecialchars($nombre) . "<br>";
+            $cuerpo .= "Email: " . htmlspecialchars($email) . "<br>";
+            $cuerpo .= "Telefono: " . htmlspecialchars($telefono) . "<br>";
+            $cuerpo .= "Mensaje: " . htmlspecialchars($mensaje);
+
+            // Enviar el correo
+            if (mail($dest, $asunto, $cuerpo, $headers)) {
+                echo "<center><img src='images/contacto-exito.png' class='animated fadeInRight img-responsive'></center>";
+            } else {
+                echo "<center>Error al enviar el mensaje. Por favor, inténtelo de nuevo más tarde.</center>";
+            }
+        } else {
+            echo "<center>Correo electrónico no válido. Por favor, revise y vuelva a intentarlo.</center>";
+        }
+    } else {
+        echo "<center>Por favor, complete todos los campos.</center>";
     }
-    ?>
+} else {
+    echo "<center>Error al intentar enviar los datos, verificar espacios vacíos.</center>";
+}
+?>
